5f9a6c271312d1c6b0e1fa5e0307107f2339eec8
Precious Mosetlha <caseymosetlha@gmail.com>
2025-01-18 05:44:46 +0200

    fourth commit

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
   let quiz = document.querySelector('input[name="quiz"]:checked');
   let userAnswer = quiz.value;
   if (userAnswer === correctAnswer) {
-    document.getElementById("feedback").textContent = "Correct! Well done";
+    document.getElementById("feedback").textContent = "Correct! Well done.";
   } else {
     document.getElementById("feedback").textContent =
       "That's incorrect. Try again";
