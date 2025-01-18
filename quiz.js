20bd5664e6916c43f7fa486c66eeff4e65a60682
Precious Mosetlha <caseymosetlha@gmail.com>
2025-01-18 05:48:14 +0200

    sixth commit

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ function checkAnswer() {
     document.getElementById("feedback").textContent = "Correct! Well done.";
   } else {
     document.getElementById("feedback").textContent =
-      "That's incorrect. Try again";
+      "That's incorrect. Try again!";
   }
 }
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
