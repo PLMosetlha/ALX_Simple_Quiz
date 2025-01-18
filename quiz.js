9aebb027eef412923ba6d3af0f1017296cb4baf0
Precious Mosetlha <caseymosetlha@gmail.com>
2025-01-18 04:22:22 +0200

    second commit

--- quiz.js
+++ quiz.js
@@ -4,9 +4,9 @@ function checkAnswer() {
   let userAnswer = quiz.value;
   if (userAnswer === correctAnswer) {
     document.getElementById("feedback").textContent = "Correct! Well done";
-  } else {
+  } else if (userAnswer !== correctAnswer) {
     document.getElementById("feedback").textContent =
-      "Thata incorrect. Try again!";
+      "Thats incorrect. Try again!";
   }
 }
 let myButton = document.getElementById("submit-answer");
